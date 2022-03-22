30bb55e1e3d0030e338145c75fb4e336c27be206
taylor <thowell@stanford.edu>
2022-03-22 13:32:19 -0700

    match cartpole quickstart example

--- examples/cartpole/cartpole.jl
+++ examples/cartpole/cartpole.jl
@@ -9,7 +9,7 @@ using Plots
 using DirectTrajectoryOptimization 
 
 # ## horizon 
-T = 101 
+T = 101
 
 # ## cartpole 
 num_state = 4 
@@ -29,28 +29,35 @@ function cartpole(x, u, w)
     c = cos(q[2])
 
     H = [mc+mp mp*l*c; mp*l*c mp*l^2]
-    Hinv = 1.0 / (H[1, 1] * H[2, 2] - H[1, 2] * H[2, 1]) * [H[2, 2] -H[1, 2]; -H[2, 1] H[1, 2]]
+    Hinv = 1.0 / (H[1, 1] * H[2, 2] - H[1, 2] * H[2, 1]) * [H[2, 2] -H[1, 2]; -H[2, 1] H[1, 1]]
+    
     C = [0 -mp*qd[2]*l*s; 0 0]
     G = [0, mp*g*l*s]
     B = [1, 0]
 
     qdd = -Hinv * (C*qd + G - B*u[1])
 
+
     return [qd; qdd]
 end
 
-function rk3_explicit(y, x, u, w)
+function rk3_explicit(x, u, w)
     h = 0.05 # timestep 
 
     k1 = h * cartpole(x, u, w)
     k2 = h * cartpole(x + 0.5 * k1, u, w)
     k3 = h * cartpole(x - k1 + 2.0 * k2, u, w)
     
-    return y - (x + (k1 + 4.0 * k2 + k3) / 6.0)
+    return x + (k1 + 4.0 * k2 + k3) / 6.0
+end
+
+function rk3_implicit(y, x, u, w)
+    return y - rk3_explicit(x, u, w)
 end
 
 # ## model
-dt = Dynamics(rk3_explicit, num_state, num_state, num_action, num_parameter=num_parameter)
+dt = Dynamics(rk3_implicit, num_state, num_state, num_action, 
+    num_parameter=num_parameter)
 dyn = [dt for t = 1:T-1] 
 
 # ## initialization
@@ -62,8 +69,8 @@ Q = 1.0e-2
 R = 1.0e-1 
 Qf = 1.0e2 
 
-ot = (x, u, w) -> 0.5 * Q * dot(x, x) + 0.5 * R * dot(u, u)
-oT = (x, u, w) -> 0.5 * Qf * dot(x, x)
+ot = (x, u, w) -> 0.5 * Q * dot(x - xT, x - xT) + 0.5 * R * dot(u, u)
+oT = (x, u, w) -> 0.5 * Qf * dot(x - xT, x - xT)
 ct = Cost(ot, num_state, num_action, 
     num_parameter=num_parameter)
 cT = Cost(oT, num_state, 0, 
@@ -72,31 +79,33 @@ obj = [[ct for t = 1:T-1]..., cT]
 
 # ## constraints
 u_bnd = 3.0
-bnd1 = Bound(num_state, num_action, 
-    state_lower=x1, 
-    state_upper=x1, 
+bnd1 = Bound(num_state, num_action,
     action_lower=[-u_bnd], 
     action_upper=[u_bnd])
 bndt = Bound(num_state, num_action,
     action_lower=[-u_bnd], 
     action_upper=[u_bnd])
-bndT = Bound(num_state, 0, 
-    state_lower=xT, 
-    state_upper=xT)
+bndT = Bound(num_state, 0)
 bounds = [bnd1, [bndt for t = 2:T-1]..., bndT]
 
-goal(x, u, w) = x - xT
-cons = [[Constraint() for t = 1:T-1]..., Constraint(goal, num_state, 0)]
+cons = [
+            Constraint((x, u, w) -> x - x1, num_state, num_action), 
+            [Constraint() for t = 2:T-1]..., 
+            Constraint((x, u, w) -> x - xT, num_state, 0)
+       ]
 
 # ## problem 
 p = Solver(dyn, obj, cons, bounds,
     options=Options{Float64}())
 
 # ## initialize
-x_interpolation = linear_interpolation(x1, xT, T)
 u_guess = [0.01 * ones(num_action) for t = 1:T-1]
+x_rollout = [x1] 
+for t = 1:T-1 
+    push!(x_rollout, rk3_explicit(x_rollout[end], u_guess[t], zeros(num_parameter)))
+end
 
-initialize_states!(p, x_interpolation)
+initialize_states!(p, x_rollout)
 initialize_controls!(p, u_guess)
 
 # ## solve
